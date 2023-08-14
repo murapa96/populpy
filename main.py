030e7f51c73e660976928d2d735874d6c8de4272
Pablo <p.ramos@nigal.net>
2023-08-14 23:27:23 +0200

    feat: cleaned code and added example.env

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 import os
-from dotenv import load_dotenv
 import argparse
 import csv
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import logging
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Load variables from .env file
 load_dotenv()
@@ -25,7 +29,7 @@ def get_google_search_results(query, api_key, cx, country):
         search_results = [(item["title"], item["link"]) for item in items]
         return search_results
     else:
-        print(f"Error: {response.status_code}")
+        logger.error(f"Error: {response.status_code}")
         return []
 
 
@@ -40,7 +44,7 @@ def get_google_search_trends(query, api_key, country):
             trend = trends[0]["trendingSearches"][0]
             return trend["title"]["query"], trend["formattedTraffic"]
     else:
-        print(f"Error: {response.status_code}")
+        logger.error(f"Error: {response.status_code}")
 
     return query, "N/A"
 
@@ -55,7 +59,7 @@ def get_google_common_searches(query, api_key, cx, country):
         if queries:
             return [q["title"] for q in queries]
     else:
-        print(f"Error: {response.status_code}")
+        logger.error(f"Error: {response.status_code}")
 
     return []
 
@@ -81,18 +85,19 @@ def main():
                         help="Path to save the wordcloud image")
     args = parser.parse_args()
 
-    query = args.query
-    country = args.country
+    parsed_query = args.query
+    parsed_country = args.country
 
     search_results = get_google_search_results(
-        query, google_search_api_key, custom_search_engine_id, country)
+        parsed_query, google_search_api_key, custom_search_engine_id, parsed_country)
     trend_query, trend_popularity = get_google_search_trends(
-        query, google_trends_api_key, country)
+        parsed_query, google_trends_api_key, parsed_country)
     common_searches = get_google_common_searches(
-        query, google_search_api_key, custom_search_engine_id, country)
+        parsed_query, google_search_api_key, custom_search_engine_id, parsed_country)
 
     if args.wordcloud:
-        create_wordcloud(common_searches, args.wordcloud)
+        parsed_wordcloud_path = args.wordcloud
+        create_wordcloud(common_searches, parsed_wordcloud_path)
 
     # Export search results, trend popularity and common searches to CSV
     with open('results.csv', mode='w') as file:
@@ -108,16 +113,16 @@ def main():
         for idx, search in enumerate(common_searches, 1):
             writer.writerow([f"{idx}. {search}"])
 
-    print("Resultados de búsqueda de Google:")
+    logger.info("Resultados de búsqueda de Google:")
     for idx, result in enumerate(search_results, 1):
-        print(f"{idx}. {result[0]} - {result[1]}")
+        logger.info(f"{idx}. {result[0]} - {result[1]}")
 
-    print("\nPopularidad en Google Trends:")
-    print(f"{trend_query} - {trend_popularity}")
+    logger.info("\nPopularidad en Google Trends:")
+    logger.info(f"{trend_query} - {trend_popularity}")
 
-    print("\nBúsquedas comunes en Google:")
+    logger.info("\nBúsquedas comunes en Google:")
     for idx, search in enumerate(common_searches, 1):
-        print(f"{idx}. {search}")
+        logger.info(f"{idx}. {search}")
 
 
 if __name__ == "__main__":
